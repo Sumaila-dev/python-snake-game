84605e76feb9681e90ab17d997f3aefc80d650d2
Sumaila-dev <82516089+Sumaila-dev@users.noreply.github.com>
2023-07-03 11:40:21 +0000

    modules imported and screen initialized

---
+++ main.py
@@ -0,0 +1,11 @@
+from turtle import Turtle,Screen
+import time
+
+screen = Screen()
+screen.bgcolor("black")
+screen.title("My Snake Game")
+screen.setup(width=600,height=600)
+screen.tracer(0)
+
+
+
