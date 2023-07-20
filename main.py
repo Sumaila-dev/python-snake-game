b850921ed02c9e390f0021aa3f127123e274690c
Sumaila-dev <82516089+Sumaila-dev@users.noreply.github.com>
2023-07-20 22:02:13 +0000

    highscore added

--- main.py
+++ main.py
@@ -35,13 +35,11 @@ while game_is_on:
 
     #Detect collision with boundary
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() >280 or snake.head.ycor()<-280:
-        game_is_on = False
-        scoreboard.game_over()
+        scoreboard.reset()
 
     #Detect collision with tail
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
+            scoreboard.reset()
 
 screen.exitonclick()
